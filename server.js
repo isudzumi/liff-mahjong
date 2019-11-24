0caee7bee268ec3838081473eda3e49092821867
isudzumi <isudzumi@gmail.com>
2019-11-24 21:54:45 +0900

    Implement server side

---
+++ server.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const sharp = require('sharp')
+const { myLiffId } = process.env
+
+const STATIC_DIR = 'build'
+
+const removeFiles = (files) => {
+    files.forEach((fileName) => {
+        fs.unlink(`./${STATIC_DIR}/${fileName}`, (err) => {
+            if(err) {
+                console.error('Fail to remove image')
+                throw err;
+            }
+            console.log(`Successfully remove ${fileName}`)
+        })
+    })
+}
+
+app.use(express.static(STATIC_DIR))
+app.use(express.json())
+
+app.get('/send-id', (req, res) => {
+    res.json({ id: myLiffId })
+})
+
+app.post('/upload', (req, res) => {
+    const { type, image } = req.body
+    if(type !== 'base64') {
+        res.status(500)
+    }
+    const buf = Buffer.from(image, type)
+    const baseFileName = image.substring(0, 5)
+    const originalFileName = `${baseFileName}.jpg`
+    const thumbnailFileName = `${baseFileName}-thumbnail.jpg`
+    sharp(buf)
+        .toFile(`./${STATIC_DIR}/${originalFileName}`, (err, info) => {
+            if(err) {
+                console.error(err)
+                res.status(500)
+            }
+            console.info(info)
+        })
+    sharp(buf)
+        .resize(240, 240)
+        .toFile(`./${STATIC_DIR}/${thumbnailFileName}`, (err, info) => {
+            if(err) {
+                console.error(err)
+                res.status(500)
+            }
+            console.info(info)
+        })
+    setTimeout(removeFiles, 10 * 1000, [originalFileName, thumbnailFileName])
+    res.json({
+        imageUrl: originalFileName,
+        thumbnailUrl: thumbnailFileName
+    })
+})
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('app listening')
+})
