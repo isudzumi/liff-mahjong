d2f7b72b002973494beb775866bffc0c57f609f0
isudzumi <isudzumi@gmail.com>
2019-11-26 07:16:41 +0900

    Update /upload
    Change image process to using imgur api

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const fs = require('fs')
 const path = require('path')
-const qs = require('querystring')
 const express = require('express')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
 const app = express()
 const sharp = require('sharp')
-const { myLiffId } = process.env
 
 const STATIC_DIR = 'build'
+const IMGUR_UPLOAD_IMAGE_API_ENDPOINT = 'https://api.imgur.com/3/upload'
+const { MY_LIFF_ID, IMGUR_CLIENT_ID } = process.env
 
-const removeFiles = (files) => {
-    files.forEach((fileName) => {
-        fs.unlink(`./${STATIC_DIR}/${fileName}`, (err) => {
-            if(err) {
-                console.error('Fail to remove image')
-                throw err;
-            }
-            console.log(`Successfully remove ${fileName}`)
-        })
-    })
+
+const createFormData = async (buffer) => {
+    const formData = new FormData()
+    formData.append('type', 'file')
+    formData.append('image', buffer)
+    return formData
+}
+
+const uploadToImgur = async (formData) => {
+    const imageData = await fetch(IMGUR_UPLOAD_IMAGE_API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            Authorization: `Client-ID ${IMGUR_CLIENT_ID}`
+        },
+        body: formData
+    }).then(res => res.json())
+    return imageData
 }
 
 app.use(express.static(STATIC_DIR))
@@ -28,40 +36,43 @@ app.get('/', (req, res) => {
 })
 
 app.get('/send-id', (req, res) => {
-    res.json({ id: myLiffId })
+    res.json({ id: MY_LIFF_ID })
 })
 
-app.post('/upload', (req, res) => {
+app.post('/upload', async (req, res) => {
     const { type, image } = req.body
     if(type !== 'base64') {
         res.status(500)
     }
-    const buf = Buffer.from(image, type)
-    const baseFileName = qs.escape(image.substring(0, 5))
-    const originalFileName = `${baseFileName}.jpg`
-    const thumbnailFileName = `${baseFileName}-thumbnail.jpg`
-    sharp(buf)
-        .toFile(`./${STATIC_DIR}/${originalFileName}`, (err, info) => {
-            if(err) {
-                console.error(err)
-                res.status(500)
-            }
-            console.info(info)
+    try {
+        const buf = Buffer.from(image, type)
+        const thumbnailBuf = await sharp(buf)
+            .resize(240, 240)
+            .toBuffer()
+
+        const original =  await createFormData(buf)
+            .then((data) => uploadToImgur(data))
+        const thumbnail =  await createFormData(thumbnailBuf)
+            .then((data) => uploadToImgur(data))
+
+        if(!original.success) {
+            console.error(original)
+            res.status(original.status)
+        }
+
+        if(!thumbnail.success) {
+            console.error(thumbnail)
+            res.status(thumbnail.status)
+        }
+
+        res.json({
+            imageUrl: original.data.link,
+            thumbnailUrl: thumbnail.data.link
         })
-    sharp(buf)
-        .resize(240, 240)
-        .toFile(`./${STATIC_DIR}/${thumbnailFileName}`, (err, info) => {
-            if(err) {
-                console.error(err)
-                res.status(500)
-            }
-            console.info(info)
-        })
-    setTimeout(removeFiles, 10 * 1000, [originalFileName, thumbnailFileName])
-    res.json({
-        imageUrl: originalFileName,
-        thumbnailUrl: thumbnailFileName
-    })
+    } catch (error) {
+        console.error(error)
+        res.status(500)
+    }
 })
 
 app.listen(process.env.PORT || 3001, () => {
